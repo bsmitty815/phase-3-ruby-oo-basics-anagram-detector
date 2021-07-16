464ea77a1c687814a748eebd9170b9d01a7b05a5
Brian Smith <brian.bubbam@gmail.com>
2021-07-16 12:51:20 -0500

    commit tree 7999e713186a347468d86388922cbc4111920c3a

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+require "pry"
+class Anagram
+
+    attr_reader :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(array)
+    
+        array.select {|word| word.chars.sort == @word.chars.sort} #mapping throuhg the array and then taking the word
+        #making the letters strings and sorting them. Then comparing them to the word set in the initializer
+
+    end
+
+
+end
